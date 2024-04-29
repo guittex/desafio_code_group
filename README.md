b4e2ccf6cee04724063dbc51425eceb2bd1e3799
guittex <guilherme.oliveira@asprev.com.br>
2024-04-29 20:32:24 -0300

    Manual de instalacao

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-## Sobre o projeto
+## Passos de instalação
 
-O projeto é um sistema com um CRUD de produtos feito com laravel usando tema Admin LTE, no CRUD é possível:
-
-- Visualizar produtos.
-- Adicionar produtos.
-- Editar produtos.
-- Remover Produtos.
-
-## Licença
-[MIT license](https://opensource.org/licenses/MIT).
+- Executar o comando para clonar o projeto: git clone https://github.com/guittex/desafio_code_group.git
+- Rodar o comando "composer install"
+- Renomear o arquivo ".env.example" para ".env"
+- Crie um banco chamdo "code_group"
+- Execute o comando "php artisan migrate"
+- Altere o arquivo .env para as configurações de sua máquina
+- Execute o comando "php artisan serve" para executar o sistema
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7268de98ff8ce9a9d8d290fb30e922c7f28713bc',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -458,9 +458,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7268de98ff8ce9a9d8d290fb30e922c7f28713bc',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
